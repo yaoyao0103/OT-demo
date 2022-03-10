968df04acd2cc14ed431fa7b7176ae196e6c7d74
Yao <elites96105@gmail.com>
2022-03-10 19:57:18 +0800

    feat: first commit

---
+++ Op.js
@@ -0,0 +1,9 @@
+class Op {
+    constructor(UId, opName, parentId, index, content){
+        this.UId = UId;
+        this.opName = opName;
+        this.parentId = parentId;
+        this.index = index;
+        this.content = content;
+    }
+}
